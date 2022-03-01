8ac9c8e17f5de477f66a0f6a953577a04607dfaf
Sigfried Gold <sigfried@jhu.edu>
2022-03-01 15:04:33 -0500

    ready to import Lisa2 with GRAVITY SDoH vsets and CDC condition vsets

--- scripts/old-vsac-api-scripts/get_tgt.sh
+++ scripts/old-vsac-api-scripts/get_tgt.sh
@@ -3,13 +3,12 @@
 GIT_ROOT=$(git rev-parse --show-toplevel)
 . "${GIT_ROOT}/env/.env"
 
-if ! find "tgt.txt" -mmin +480 ; then
-  :
-  # echo "ticket still fresh"
+if test `find "tgt.txt" -mmin -1` ; then
+  >&2 echo "ticket still fresh"
 else
-  # echo "fetching new ticket"
+  >&2 echo "fetching new ticket"
   # curl -w '\n' --data "username=$VSAC_API_USER&password=$VSAC_API_PWD"  https://vsac.nlm.nih.gov/vsac/ws/Ticket > tgt.txt
-  curl -w '\n' --data "apikey=$VSAC_API_KEY"  https://vsac.nlm.nih.gov/vsac/ws/Ticket >| tgt.txt
+  curl -w '\n' --data "apikey=$VSAC_API_KEY"  https://vsac.nlm.nih.gov/vsac/ws/Ticket > tgt.txt
 fi
 TGT=`cat tgt.txt`
 echo $TGT
