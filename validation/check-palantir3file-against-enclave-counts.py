d17d4d5af474543f229d6ee3e1d6d54904706290
Sigfried Gold <sigfried@jhu.edu>
2022-03-07 19:25:05 -0500

    getting closer to cataloguing the differences

--- validation/check-palantir3file-against-enclave-counts.py
+++ validation/check-palantir3file-against-enclave-counts.py
@@ -17,7 +17,13 @@ def quick_and_dirty():
     versions, csets, items = [pd.concat([vsac_dfs[i], hcup_dfs[i]]) for i in range(3)]
 
     counts = pd.read_csv('data/Lisa3forMarch9Bonanza/code_counts_back_from_enclave.csv', keep_default_na=False)
-    counts['prefix'] = counts.concept_set_name.str.replace('\].*', ']', regex=True)
+    cols = [c for c in counts.columns]
+    cols[1] = 'concept_set_name'
+    counts.columns = cols
+
+    counts['prefix']     =     counts.concept_set_name.str.replace('\].*', ']', regex=True)
+    v_versions['prefix'] = v_versions.concept_set_name.str.replace('\].*', ']', regex=True)
+    h_versions['prefix'] = h_versions.concept_set_name.str.replace('\].*', ']', regex=True)
 
     ann_pre = {x[0]: x[1].groupby('annotation') for x in counts.groupby('annotation')}
 
@@ -26,18 +32,38 @@ def quick_and_dirty():
         for ann, gg in g.groupby('annotation'):
             print(f'    {ann}   {len(gg)}')
 
-    try:
+        z = dict(zip(['vsac_enclave', 'vsac_csv', 'hcup_enclave', 'hcup_csv', ], [
+            set(counts[counts.prefix == '[VSAC]'].concept_set_name),
+            set(v_versions[v_versions.prefix == '[VSAC]'].concept_set_name),
+            set(counts[counts.prefix == '[HCUP]'].concept_set_name),
+            set(h_versions[h_versions.prefix == '[HCUP]'].concept_set_name),
+        ]))
+
+        for k,v in z.items():
+            print(f'sets in {k}: {len(v)}')
+
         print(f"vsac enclave sets: {len(set(counts[counts.prefix == '[VSAC]'].concept_set_name))}")
-        print(f"vsac csv sets: {len(set(v_versions.concept_set_name)}")
+        print(f"vsac csv sets: {len(set(v_versions[v_versions.prefix == 'VSAC']))}")
         print(f"hcup enclave sets: {len(set(counts[counts.prefix == '[HCUP]'].concept_set_name))}")
-        print(f"hcup csv sets: {len(set(h_versions.concept_set_name)")
-        assert len(set(counts[counts.prefix == '[VSAC]'].concept_set_name) - set(v_versions.concept_set_name))
-        assert len(set(v_versions.concept_set_name) - set(counts[counts.prefix == '[VSAC]'].concept_set_name))
-        assert len(set(counts[counts.prefix == '[HCUP]'].concept_set_name) - set(h_versions.concept_set_name))
-        assert len(set(h_versions.concept_set_name) - set(counts[counts.prefix == '[HCUP]'].concept_set_name))
-    except AssertionError as ae:
-        print("\n".join(set(v_versions.concept_set_name) - set(counts[counts.prefix == '[VSAC]'].concept_set_name)))
-        print(ae)
+        print(f"hcup csv sets: {len(set(v_versions[v_versions.prefix == 'HCUP']))}")
+        #print(f"hcup csv sets: {len(set(h_versions.concept_set_name)")
+
+
+        'vsac_enclave', 'vsac_cs', 'hcup_enclav', 'hcup_cs',
+        set(counts[counts.prefix == '[VSAC]'].concept_set_name)
+        set(v_versions[v_versions.prefix == '[VSAC]'].concept_set_name)
+        set(counts[counts.prefix == '[HCUP]'].concept_set_name)
+        set(v_versions[v_versions.prefix == '[HCUP]'].concept_set_name)
+
+        try:
+            hcup_csv = set(v_versions.concept_set_name)
+            assert len(set(counts[counts.prefix == '[VSAC]'].concept_set_name) - set(v_versions.concept_set_name))
+            assert len(set(v_versions.concept_set_name) - set(counts[counts.prefix == '[VSAC]'].concept_set_name))
+            assert len(set(counts[counts.prefix == '[HCUP]'].concept_set_name) - set(h_versions.concept_set_name))
+            assert len(set(h_versions.concept_set_name) - set(counts[counts.prefix == '[HCUP]'].concept_set_name))
+        except AssertionError as ae:
+            print("\n".join(set(v_versions.concept_set_name) - set(counts[counts.prefix == '[VSAC]'].concept_set_name)))
+            print(ae)
 
 
 
