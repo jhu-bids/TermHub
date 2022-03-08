67c555fbff74a59ba0c2c2cfd12fc2455878ff23
Sigfried Gold <sigfried@jhu.edu>
2022-03-08 07:03:33 -0500

    - to the point that I can match everything from the CSVs to sets on the   enclave. there are still a bunch of concept_set_names on the enclave   that didn't come from Lisa3, HCUP, or the old VSAC Bulk-Import test - fixed all that with new function `strip` - kept messed up names in `concept_set_name` and put fixed names in   `cset_id` - created a lookup `codesetid2name` so I can figure out which items   from the CSVs are supposed to match which from the enclave (where   our _internal_ codeset_id is not to be found.)

--- validation/check-palantir3file-against-enclave-counts.py
+++ validation/check-palantir3file-against-enclave-counts.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import re
 
 THREE_FILES = ('code_sets.csv', 'concept_set_container_edited.csv', 'concept_set_version_item_rv_edited.csv')
 VSAC_PATH = 'data/palantir-three-file/Lisa3/2022.03.02/output'
@@ -8,22 +9,56 @@ HCUP_PATH = 'data/palantir-three-file/hcup/2022.03.02/output'
 def load3file(dirpath):
     return (pd.read_csv(os.path.join(dirpath, file)) for file in THREE_FILES)
 
+
+def strip(s: str) -> str:
+    s = re.sub(r'^[·\s]+', '', s)
+    s = re.sub(r'[·\s]+$', '', s)
+    s = re.sub(r'  +', ' ', s)
+    return s
+
+
 def quick_and_dirty():
     v_versions, v_csets, v_items = load3file(VSAC_PATH)
     vsac_dfs = [v_versions, v_csets, v_items]
     h_versions, h_csets, h_items = load3file(HCUP_PATH)
     hcup_dfs = [h_versions, h_csets, h_items]
-
     versions, csets, items = [pd.concat([vsac_dfs[i], hcup_dfs[i]]) for i in range(3)]
 
     counts = pd.read_csv('data/Lisa3forMarch9Bonanza/code_counts_back_from_enclave.csv', keep_default_na=False)
-    cols = [c for c in counts.columns]
-    cols[1] = 'concept_set_name'
-    counts.columns = cols
 
-    counts['prefix']     =     counts.concept_set_name.str.replace('\].*', ']', regex=True)
+    # some of this code was written just to figure out what was going on with all this messed up data and how to fix it
+    # part of what stumped me for a long time. A bunch of the enclave concept_set_names have an ASCII 183 at the end
+    # which looks like this:  ·  , a period in the middle of the line. don't ask me why
+
+    # another: the enclave unhelpfully "fixed" our double spaces so  '[VSAC] Bells Palsy,  codes for AE reporting'
+    # in the CSV ended up '[VSAC] Bells Palsy, codes for AE reporting' on the enclave
+
+    counts['prefix'] = counts.cset_id.str.replace('\].*', ']', regex=True)
     v_versions['prefix'] = v_versions.concept_set_name.str.replace('\].*', ']', regex=True)
     h_versions['prefix'] = h_versions.concept_set_name.str.replace('\].*', ']', regex=True)
+    versions['prefix'] = versions.concept_set_name.str.replace('\].*', ']', regex=True)
+
+    counts['concept_set_name'] = list(counts['cset_id'])
+    counts['cset_id'] = counts.concept_set_name.apply(strip)
+    counts['whitespace'] = counts.concept_set_name == counts.cset_id
+
+    versions['cset_id'] = versions.concept_set_name.apply(strip)
+    csets['cset_id'] = csets.concept_set_name.apply(strip)
+
+    counts = counts[counts.prefix != '[VSAC Bulk-Import test]']
+
+    missing_from_enclave = set(versions.cset_id) - set(counts.cset_id)
+    extra_on_enclave = set(counts.cset_id) - set(versions.cset_id)
+
+    s_missing_from_enclave = set(n.strip() for n in missing_from_enclave)
+    s_extra_on_enclave = set(n.strip() for n in extra_on_enclave)
+
+    [g for g in pd.DataFrame({'w': [n == n.strip() for n in versions.concept_set_name], 'n': versions.concept_set_name,
+                              's': [n.strip() for n in versions.concept_set_name]}).groupby('w')]
+
+    codesetid2name = dict(zip(versions.codeset_id, versions.concept_set_name))
+
+
 
     ann_pre = {x[0]: x[1].groupby('annotation') for x in counts.groupby('annotation')}
 
@@ -55,6 +90,11 @@ def quick_and_dirty():
         set(counts[counts.prefix == '[HCUP]'].concept_set_name)
         set(v_versions[v_versions.prefix == '[HCUP]'].concept_set_name)
 
+        hcup_dfs[2].merge(hcup_dfs[1], left_on="codeset_id", right_on="concept_set_id", how="inner").groupby(['concept_set_name', 'codeset_id']).size()
+           # .merge(z['hcup_enclave'], on='concept_set_name')
+        pd.DataFrame(
+            hcup_dfs[2].merge(hcup_dfs[1], left_on="codeset_id", right_on="concept_set_id", how="inner").groupby(
+                ['concept_set_name', 'codeset_id']).size()).reset_index().merge(z['hcup_enclav'], on='concept_set_name')
         try:
             hcup_csv = set(v_versions.concept_set_name)
             assert len(set(counts[counts.prefix == '[VSAC]'].concept_set_name) - set(v_versions.concept_set_name))
