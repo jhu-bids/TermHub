d4641903a37dbcb81c2607c858e6c31d0af2410c
Sigfried Gold <sigfried@jhu.edu>
2022-04-18 16:47:11 -0400

    getting ready to do some dataset_download

---
+++ enclave_wrangler/utils.py
@@ -0,0 +1,37 @@
+"""Extra utilities"""
+import logging
+from http.client import HTTPConnection
+import contextlib
+
+
+
+def log_debug_info():
+    """Logs additional info when making HTTP requests"""
+    # These two lines enable debugging at httplib level (requests->urllib3->http.client)
+    # You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+    # The only thing missing will be the response.body which is not logged.
+    try:
+        import http.client as http_client
+    except ImportError:
+        # Python 2
+        import httplib2 as http_client
+    http_client.HTTPConnection.debuglevel = 1
+
+    # You must initialize logging, otherwise you'll not see debug output.
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+
+def debug_requests_off():
+    # ''' Switches off logging of the requests module, might be some side-effects '''
+    HTTPConnection.debuglevel = 0
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.WARNING)
+    root_logger.handlers = []
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.WARNING)
+    requests_log.propagate = False
