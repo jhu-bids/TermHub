0454d413f208841352e1b4585fe59b096ed178f5
joeflack4 <joeflack4@gmail.com>
2022-07-08 14:28:44 -0400

    - Added feature: FHIR Terminology page - Added feature: N3C data enclave page - Added: makefile: mainly for orchestrating internal packages (e.g. enclave_wrangler) - Added .run/ for PyCharm runtime configs

---
+++ makefile
@@ -0,0 +1,87 @@
+SRC=termhub/
+
+.PHONY: lint tags ltags test all lintall codestyle docstyle lintsrc \
+linttest doctest doc docs code linters_all codesrc codetest docsrc \
+doctest build dist pypi-push-test pypi-push pypi-test pip-test pypi \
+pip remove-previous-build upgrade upgrade-dependencies
+
+# Batched Commands
+# - Code & Style Linters
+all: linters_all testall
+lint: lintsrc codesrc docsrc
+linters_all: doc code lintall
+
+# Pylint Only
+PYLINT_BASE =python3 -m pylint --output-format=colorized --reports=n
+lintall: lintsrc linttest
+lintsrc:
+	${PYLINT_BASE} ${SRC}
+linttest:
+	${PYLINT_BASE} test/
+
+# PyCodeStyle Only
+PYCODESTYLE_BASE=python3 -m pycodestyle
+codestyle: codestylesrc codestyletest
+codesrc: codestylesrc
+codetest: codestyletest
+code: codestyle
+codestylesrc:
+	${PYCODESTYLE_BASE} ${SRC}
+codestyletest:
+	 ${PYCODESTYLE_BASE} test/
+
+# PyDocStyle Only
+PYDOCSTYLE_BASE=python3 -m pydocstyle
+docstyle: docstylesrc docstyletest
+docsrc: docstylesrc
+doctest: docstyletest
+docs: docstyle
+docstylesrc:
+	${PYDOCSTYLE_BASE} ${SRC}
+docstyletest:
+	${PYDOCSTYLE_BASE} test/
+codetest:
+	python -m pycodestyle test/
+codeall: code codetest
+doc: docstyle
+
+# Testing
+test:
+	python3 -m unittest discover -v
+testdoc:
+	python3 -m test.test --doctests-only
+testall: test testdoc
+
+# Dependency management
+upgrade: upgrade-dependencies
+# todo: make sure 1 or both of these work
+upgrade-dependencies:
+	python3 -m pip install --no-cache-dir --upgrade -r requirements-unlocked.txt; \
+	pip freeze > requirements.txt
+upgrade-dependencies-specifically:
+	python3 -m pip install --upgrade \
+	https://github.com/jhu-bids/valueset-tools/zipball/master
+
+# Package management
+remove-previous-build:
+	rm -rf ./dist; 
+	rm -rf ./build; 
+	rm -rf ./*.egg-info
+build: remove-previous-build
+	python3 setup.py sdist bdist_wheel
+dist: build
+pypi-push-test: build
+	twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+pypi-push:
+	twine upload --repository-url https://upload.pypi.org/legacy/ dist/*; \
+	make remove-previous-build
+pypi-test: pypi-push-test
+pip-test: pypi-push-test
+pypi: pypi-push
+pip: pypi-push
+
+# Serve
+serve:
+	python3 -m termhub; \
+	open http://localhost:5000
+
--- termhub/app.py
+++ termhub/app.py
@@ -3,8 +3,10 @@
 Resources
   1. Features to add: https://docs.google.com/spreadsheets/d/19_eBv0MIBWPcXMTw3JJdcfPoEFhns93F-TKdODW27B8/edit#gid=0
   2. Flask boilerplate: https://github.com/realpython/flask-boilerplate
+  3. App: https://termhub.herokuapp.com
 
 TODO's:
+  - Fetch from my FHIR server and see what it looks like
   - Heroku: submodules issue: google "heroku deploy with git submodules"
   - Patient counts: Get from N3C when they give us this API.
   - Datasets: Find hosting for static files / database: (a) AWS, (b) DataBricks, (c) our FHIR server
@@ -24,6 +26,7 @@ TODO's:
 from typing import Dict
 
 import pandas as pd
+import requests
 from flask import Flask, render_template, request
 # from flask.ext.sqlalchemy import SQLAlchemy
 import logging
@@ -37,6 +40,11 @@ import os
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
+CONFIG = {
+    'fhir_api_urls': {
+    1: 'http://20.119.216.32:8080/fhir/'
+}
+}
 
 app = Flask(__name__)
 try:
@@ -87,6 +95,7 @@ def load_concept_sets(by_id: bool = False) -> Dict[str, str]:
             concept_sets[row['concept_set_name']] = row['codeset_id']
     return concept_sets
 
+
 @app.route('/')
 def home():
     concept_sets: Dict = load_concept_sets()
@@ -95,18 +104,73 @@ def home():
     return render_template('pages/concept_sets.html', concept_sets=concept_sets)
 
 
-@app.route('/concept_sets')
+@app.route('/concept-sets')
 def concept_sets():
     concept_sets: Dict = load_concept_sets()
     return render_template('pages/concept_sets.html', concept_sets=concept_sets)
 
 
 # TODO: AssertionError: View function mapping is overwriting an existing endpoint function: concept_sets
-@app.route('/concept_sets/<concept_set_id>')
+@app.route('/concept-sets/<concept_set_id>')
 def concept_set(concept_set_id: str):
     concept_sets: Dict = load_concept_sets(by_id=True)
     concept_set_name: str = concept_sets[concept_set_id]
     return render_template('pages/concept_set.html', concept_set_id=concept_set_id, concept_set_name=concept_set_name)
+
+
+@app.route('/fhir-terminology')
+def fhir_terminology():
+    return fhir_terminology_i()
+
+
+# TODO: standardize this use case. right now this is just an experiment
+@app.route('/fhir-terminology/<api_url_id>')
+def fhir_terminology_i(api_url_id: str = '1'):
+    """FHIR Terminology
+    http://20.119.216.32:8080/fhir/"""
+    # TODO: for some reason code systems etc aren't fetching from url dynamically
+    # TODO: need when select from dropdown, actually follows link. look at url_for from main.html
+    # TODO: Show only a subset of the JSON returned
+    fhir_api_urls: Dict[int, str] = CONFIG['fhir_api_urls']
+    fhir_api_base_url = fhir_api_urls[int(api_url_id)]
+    fhir_codesystem_url = fhir_api_base_url + 'CodeSystem'
+    fhir_valueset_url = fhir_api_base_url + 'ValueSet'
+    fhir_conceptmap_url = fhir_api_base_url + 'ConceptMap'
+    code_systems = requests.get(fhir_codesystem_url).json()
+    value_sets = requests.get(fhir_valueset_url).json()
+    concept_maps = requests.get(fhir_conceptmap_url).json()
+    return render_template('pages/fhir_terminology.html',
+        fhir_api_url_selection_id=int(api_url_id),
+        fhir_api_urls=fhir_api_urls,
+        code_systems=code_systems,
+        value_sets=value_sets,
+        concept_maps=concept_maps)
+
+
+# TODO: Package enclave_wrangler separately. add this functionality there, and import that
+@app.route('/enclave')
+def enclave():
+    """N3C Palantir Data Enclave
+    - https://www.palantir.com/docs/foundry/api/general/overview/paging/
+    - https://www.palantir.com/docs/foundry/api/ontology-resources/objects/list-objects/"""
+    # TODO: Utilize some of this:
+    # [3:25 PM] Sigfried Gold
+    # cat ../ValueSet-Tools-REPLACED-BY-TermHub/enclave_wrangler/curl-tests/output/conceptSetVersionItems.json|jq  '.nextPageToken'
+    # "v1.eyJ0IjoiT2JqZWN0cy4xIiwicHMiOjEwMDAsInRva2VuIjoidjEuZXlKeVpYRjFaWE4wUTJobFkydHpkVzBpT2lJMU5EQXpOalZsTldZeU9HUmhZVGc0WkROallqa3lOVFV5T0RRd01HVmhOemcxWVdZM01HSTNZVEEzTWpNME4ySmhZVEF4TlRVMllqQTRPREE0TWpBeUlpd2lZbUZqYTJWdVpGQmhaMlZVYjJ0bGJpSTZJbll4TGpVeFpqTTBPRFE1TFRnek1UQXROR0kzWkMwNE5UbGpMV1kxTTJRM05qSTJOelU1WVM0eE1EQXdJaXdpWW1GamEyVnVaQ0k2SWxCSVQwNVBSMUpCVUVnaWZRPT0ifQ=="
+    #  curl \
+    #                   -H "Content-type: application/json" \
+    #                   -H "Authorization: Bearer $TOKEN" \
+    #                   "https://$HOSTNAME/api/v1/ontologies" | jq '.data[0].rid'
+    # Header: 'Content-type: application/json'
+    # Header: 'Authorization: Bearer $TOKEN'
+
+    # hostname = 'unite.nih.gov'
+    # rid = ''
+    # ontology_rid = ''
+    # x1 = f'https://{hostname}/api/v1/ontologies/{rid}'
+    # x2 = f'https://{hostname}/api/v1/ontologies/{ontology_rid}/objects/OmopConceptSetVersionItem'
+
+    return render_template('pages/enclave.html')
 
 
 @app.route('/about')
@@ -131,9 +195,8 @@ def forgot():
     form = ForgotForm(request.form)
     return render_template('forms/forgot.html', form=form)
 
+
 # Error handlers.
-
-
 @app.errorhandler(500)
 def internal_error(error):
     #db_session.rollback()
