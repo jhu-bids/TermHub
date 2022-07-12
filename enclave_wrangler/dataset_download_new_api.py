23e14a5dcfbef76ee529380050ce8430398ca1c3
Sigfried Gold <sigfried@jhu.edu>
2022-07-12 06:42:32 -0400

    querying enclave ontology for object types

--- enclave_wrangler/dataset_download_new_api.py
+++ enclave_wrangler/dataset_download_new_api.py
@@ -27,120 +27,29 @@ HEADERS = {
     #'content-type': 'application/json'
 }
 DEBUG = False
-TARGET_CSV_DIR='data/datasets/'
+# TARGET_CSV_DIR='data/datasets/'
 
 
 @typechecked
-def getTransaction(datasetRid: str, ref: str = 'master') -> str:
+def objTypes() -> [{}]:
     """API documentation at
-    https://unite.nih.gov/workspace/documentation/developer/api/catalog/services/CatalogService/endpoints/getTransaction
-    tested with curl:
-    curl https://unite.nih.gov/foundry-catalog/api/catalog/datasets/ri.foundry.main.dataset.5cb3c4a3-327a-47bf-a8bf-daf0cafe6772/transactions/master -H "authorization: Bearer $PALANTIR_ENCLAVE_AUTHENTICATION_BEARER_TOKEN" | json_pp
+    https://www.palantir.com/docs/foundry/api/ontology-resources/object-types/list-object-types/
     """
-    if DEBUG:
-        log_debug_info()
 
-    endpoint = 'https://unite.nih.gov/foundry-catalog/api/catalog/datasets/'
-    template = '{url}{datasetRid}/transactions/{ref}'
-    url = template.format(url=endpoint, datasetRid=datasetRid, ref=ref)
+    ontologyRid = config['ONTOLOGY_RID']
+    api_path = f'/api/v1/ontologies/{ontologyRid}/objectTypes'
+    url = f'https://{config["HOSTNAME"]}{api_path}'
 
     response = requests.get(url, headers=HEADERS,)
     response_json = response.json()
     print(response_json)
-    return response_json['rid']
+    # types = pd.DataFrame(data=response_json)
+    return response_json['data']
 
-@typechecked
-def views2(datasetRid: str, endRef: str) -> [str]:
-    """API documentation at
-    https://unite.nih.gov/workspace/documentation/developer/api/catalog/services/CatalogService/endpoints/getDatasetViewFiles2
-    tested with curl:
-    curl https://unite.nih.gov/foundry-catalog/api/catalog/datasets/ri.foundry.main.dataset.5cb3c4a3-327a-47bf-a8bf-daf0cafe6772/views2/ri.foundry.main.transaction.00000022-85ed-47eb-9eeb-959737c88847/files?pageSize=100 -H "authorization: Bearer $PALANTIR_ENCLAVE_AUTHENTICATION_BEARER_TOKEN" | json_pp
-    """
-    curl_url = "https://unite.nih.gov/foundry-catalog/api/catalog/datasets/ri.foundry.main.dataset.5cb3c4a3-327a-47bf-a8bf-daf0cafe6772/views2/ri.foundry.main.transaction.00000022-85ed-47eb-9eeb-959737c88847/files?pageSize=100"
-
-    if DEBUG:
-        log_debug_info()
-
-    endpoint = 'https://unite.nih.gov/foundry-catalog/api/catalog/datasets/'
-    template = '{endpoint}{datasetRid}/views2/{endRef}/files?pageSize=100'
-    url = template.format(endpoint=endpoint, datasetRid=datasetRid, endRef=endRef)
-
-    response = requests.get(url, headers=HEADERS,)
-    response_json = response.json()
-    file_parts = [f['logicalPath'] for f in response_json['values']]
-    return file_parts[1:]
-
-@typechecked
-def datasets_views(datasetRid: str, file_parts: [str]) -> None:
-    """tested with cURL:
-    wget https://unite.nih.gov/foundry-data-proxy/api/dataproxy/datasets/ri.foundry.main.dataset.5cb3c4a3-327a-47bf-a8bf-daf0cafe6772/views/master/spark%2Fpart-00000-c94edb9f-1221-4ae8-ba74-58848a4d79cb-c000.snappy.parquet --header "authorization: Bearer $PALANTIR_ENCLAVE_AUTHENTICATION_BEARER_TOKEN"
-    """
-
-    endpoint = 'https://unite.nih.gov/foundry-data-proxy/api/dataproxy/datasets'
-    template = '{endpoint}/{datasetRid}/views/master/{fp}'
-
-    # download parquet files
-    with tempfile.TemporaryDirectory() as parquet_dir:
-        print('created temporary directory', parquet_dir)
-        parquet_parts = []
-        for fp in file_parts:
-            url = template.format(endpoint=endpoint, datasetRid=datasetRid, fp=fp)
-            response = requests.get(url, headers=HEADERS, stream=True)
-            if response.status_code == 200:
-                fname = parquet_dir + fp.replace('spark', '')
-                with open(fname, "wb") as f:
-                    response.raw.decode_content = True
-                    print(f'{fname} copying {time.strftime("%X")} ---> ', end='')
-                    shutil.copyfileobj(response.raw, f)
-                    # print(f'sleeping {time.strftime("%X")} ---> ', end='')
-                    # await asyncio.sleep(2)
-                    # print(f'waking {time.strftime("%X")} ---> ', end='')
-                    print()
-                try:
-                    print(f'reading {time.strftime("%X")} ---> ', end='')
-                    part_df = pd.read_parquet(fname)
-                    print(f'read {time.strftime("%X")}')
-                except Exception as e:
-                    print(f'errored {time.strftime("%X")} ----> {e}')
-                    raise e
-                    parquet_parts.append(fname)
-            else:
-                raise f'failed opening {url} with {response.status_code}: {response.content}'
-        combined_parquet_fname = parquet_dir + '/combined.parquet'
-        combine_parquet_files(parquet_dir, combined_parquet_fname)
-        df = pd.read_parquet(combined_parquet_fname)
-        df.to_csv()
-
-
-        # p1 = pd.read_parquet('./spark%2Fpart-00000-c94edb9f-1221-4ae8-ba74-58848a4d79cb-c000.snappy.parquet')
-
-
-
-def combine_parquet_files(input_folder, target_path):
-    try:
-        files = []
-        for file_name in os.listdir(input_folder):
-            files.append(pq.read_table(os.path.join(input_folder, file_name)))
-
-        with pq.ParquetWriter(target_path,
-                              files[0].schema,
-                              version='2.0',
-                              compression='gzip',
-                              use_dictionary=True,
-                              data_page_size=2097152,  # 2MB
-                              write_statistics=True
-        ) as writer:
-            for f in files:
-                writer.write_table(f)
-    except Exception as e:
-        print(e)
-
-def run(datasetRid: str, ref: str = 'master') -> None:
-    endRef = getTransaction(datasetRid, ref)
-    args = {'datasetRid': datasetRid, 'endRef': endRef}
-    file_parts = views2(**args)
-    datasets_views(datasetRid, file_parts)
-    # asyncio.run(datasets_views(datasetRid, file_parts))
+def run() -> None:
+    types = objTypes()
+    print('\n'.join([t['apiName'] for t in types]))
+    pass
 
 
 
@@ -159,24 +68,6 @@ def get_parser():
         default='PALANTIR_ENCLAVE_AUTHENTICATION_BEARER_TOKEN',
         help='Name of the environment variable holding the auth token you want to use')
 
-    parser.add_argument(
-        '--datasetName',
-        help='Name of enclave dataset you want to download. CSV will be saved to ValueSet-Tools/data/datasets/<name>')
-
-    parser.add_argument(
-        '--datasetRid',
-        help='RID of enclave dataset you want to download.')
-
-    parser.add_argument(
-        '--ref',
-        default='master',
-        help='Should be the branch of the dataset -- I think. Refer to API documentation at '
-                'https://unite.nih.gov/workspace/documentation/developer/api/catalog/services/CatalogService/endpoints/getTransaction')
-
-#    parser.add_argument(
-#        '-o', '--output_dir',
-#        help='Path to folder where you want output files, if there are any')
-
     return parser
 
 def cli():
@@ -188,7 +79,7 @@ def cli():
     kwargs_dict: Dict = vars(kwargs)
 
     # if kwargs_dict['dataset-download'] is not None:
-    args = {key: kwargs_dict[key] for key in ['datasetRid','ref']}
+    args = {key: kwargs_dict[key] for key in []} # ['datasetRid','ref']}
     run(**args)
     return
 
