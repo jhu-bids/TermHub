27f72c605b72ee565d560e406429435afa8744bf
joeflack4 <joeflack4@gmail.com>
2022-07-18 12:34:31 -0400

    Termhub 2nd iteration: backend api + static frontend - Deleted termhub/ folder and replaced with new, blank backend/ folder. - Deleted some .sh scripts related to VSAC / N3C. - requirements: started off with fresh dependencies, removing other ones from requirements.txt

--- apicall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ./env/.env
-
-#!/bin/bash
-
-curl \
-      -H "Content-type: application/json" \
-      -H "Authorization: Bearer $TOKEN" \
-      "https://$HOSTNAME/api/v1/ontologies/$ONTOLOGY_RID/$1"
