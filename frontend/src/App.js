a0ba93fff4e7b3e3f4317f0bf9cf77d3d08717ef
Sigfried Gold <sigfried@jhu.edu>
2022-07-19 06:29:39 -0400

    - key needs to be in top element inside loop - changed rest api so the path being sent to enclave api now goes into a   path prop in query string like: http://localhost:8000/ontocall?path=objects/OMOPConceptSet - added ag-grid. right now just have csets being displayed as table. - earlier had added routing and got that working. will put it back now   that I have the table working - routing with react-router-dom works with nested routes and the   `Outlet` component - need to learn more about react hooks - added backend run config for pycharm so running now means getting   that started and then doing `npm start` in a terminal window`

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,46 +6,73 @@ https://github.com/mui/material-ui
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-// Issue: "ListItemText not defined" in JS console, and also my IDE highlights it in a different color here,
-// ...yet when I ctrl+click "ListItemText", it opens up the source code.
+import { Link, Outlet, useParams } from "react-router-dom";
+import Box from '@mui/joy/Box';
+import List from '@mui/joy/List';
+import ListItem from '@mui/joy/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+import ListItemButton from '@mui/joy/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 
+
+// Issue: "ListItemText not defined" in JS console, and also my IDE highlights it in a different color here,
+// ...yet when I ctrl+click "ListItemText", it opens up the source code.
+// import ListItemText from '@mui/joy/ListItemText';
 
 function App() {
   return (
     <div className="App">
-      <N3CObjectTypes/>
-      <N3CObjectType/>
+      <h2>whatever</h2>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/objTypes">ObjTypes</Link> |{" "}
+        <Link to="/else">Something else</Link>
+      </nav>
+      <h3>outlet here:</h3>
+      <Outlet />
     </div>
   );
 }
+//<N3CObjectType/>
 
 // TODO: Fix: Warning: React has detected a change in the order of Hooks called by N3CObjectTypes. This will lead to bugs and errors if not fixed. For more information, read the Rules of Hooks: https://reactjs.org/link/rules-of-hooks
 // TODO: I think this is bad because considered nested function (call)
 // TODO: I think I can fix by creating a state 'selectedN3CObject' instead of using onclick()
 //  - set state in N3CObjectTypes
 //  - get state in N3CObjectType
-function N3CObjectType(objectName) {
+function N3CObjectType() {
+  let params = useParams();
+  let objectName = params.objType
+  console.log(params)
   const [n3cObject, setN3cObject] = React.useState([]);
 
   React.useEffect(() => {
-    if (!Object.keys(objectName).length === 0) {
-      fetch('http://127.0.0.1:8000/ontocall/objectTypes/' + objectName)
+    //if (!Object.keys(objectName).length === 0) {
+      let apicall = 'http://127.0.0.1:8000/ontocall?path=objectTypes/' + objectName
+      console.log(`apicall:  ${apicall}`)
+      fetch(apicall)
         .then(results => results.json())
-        .then(data => setN3cObject(data));
-    }
+        .then(data => {
+          console.log(data)
+          return setN3cObject(data);
+        });
+    //}
   }, []); // <-- Have to pass in [] here!
 
   return (
     <div>
-        {/*{n3cObject}*/}
-      <span>hello</span>
+      {n3cObject}
+      <span>I am supposed to be an N3CObjectType {objectName}</span>
     </div>
   );
+}
+
+function displayN3CObjectType(typeName) {
+  return () => <div>HERE IT IS: {typeName}</div>
 }
 
 /* https://stackoverflow.com/questions/53219113/where-can-i-make-api-call-with-hooks-in-react */
@@ -53,23 +80,31 @@ function N3CObjectTypes() {
   const [n3cObjects, setN3cObjects] = React.useState([]);
 
   React.useEffect(() => {
-    fetch('http://127.0.0.1:8000/ontocall/objectTypes')
+    fetch('http://127.0.0.1:8000/ontocall?path=objectTypes')
       .then(results => results.json())
-      .then(data => setN3cObjects(data));
+      .then(data => {
+        return setN3cObjects(data);
+      });
   }, []); // <-- Have to pass in [] here!
 
   return (
     <div>
+      <h3>wtf?</h3>
+      <Outlet />
       {/*{!data ? 'Loading...' : JSON.stringify(data)}*/}
       <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
         <nav aria-label="main mailbox folders">
           <List>
           {n3cObjects.map(x =>
-            <ListItem disablePadding>
-              <ListItemButton>
-                {/* TODO: fix warning: Warning: Each child in a list should have a unique "key" prop.*/}
-                <ListItemText key={x} primary={x}/>
-                {/*<ListItemText key={x} primary={x} onClick={N3CObjectType(x)}/>*/}
+            <ListItem key={x}>
+              <ListItemButton variant="solid">
+                <Link style={{ display: "block", margin: "1rem 0" }}
+                      to={`/objTypes/${x}`}
+                      key={x}
+                >
+                  {x}
+                </Link>
+                {/*<ListItemText key={x} primary={x} onClick={displayN3CObjectType(x)}/> */}
               </ListItemButton>
             </ListItem>
           )}
@@ -80,4 +115,8 @@ function N3CObjectTypes() {
   );
 }
 
-export default App;
+function Else() {
+  return <h1>else?</h1>
+}
+
+export {App, N3CObjectTypes, N3CObjectType, Else};
