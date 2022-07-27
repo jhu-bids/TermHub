1cf776ef299d742abf0b07375f931c523ee5c6d1
Sigfried Gold <sigfried@jhu.edu>
2022-07-27 10:22:32 -0400

    committing late, don't know what changed

--- backend/app.py
+++ backend/app.py
@@ -34,12 +34,14 @@ def ontocall(path) -> [{}]:
     https://www.palantir.com/docs/foundry/api/ontology-resources/object-types/list-object-types/
     """
     headers = {
-        "authorization": f"Bearer {config['PALANTIR_ENCLAVE_AUTHENTICATION_BEARER_TOKEN']}",
+        # "authorization": f"Bearer {config['PALANTIR_ENCLAVE_AUTHENTICATION_BEARER_TOKEN']}",
+        "authorization": f"Bearer {config['PERSONAL_ENCLAVE_TOKEN']}",
         # 'content-type': 'application/json'
     }
     # return {'path': path}
     print(f'ontocall param: {path}\n')
-    ontology_rid = config['ONTOLOGY_RID']
+    # ontology_rid = config['ONTOLOGY_RID']
+    ontology_rid = 'ri.ontology.main.ontology.00000000-0000-0000-0000-000000000000'
     api_path = f'/api/v1/ontologies/{ontology_rid}/{path}'
     url = f'https://{config["HOSTNAME"]}{api_path}'
     print(f'ontocall: {api_path}\n{url}')
@@ -54,6 +56,7 @@ def ontocall(path) -> [{}]:
             data = json
         if 'properties' in data:
             data = data['properties'] # e.g., http://127.0.0.1:8000/ontocall?path=objects/OMOPConceptSet/729489911
+            data['rid'] = json['rid']
     except BaseException as err:
         print(f"Unexpected {err=}, {type(err)=}")
         return {'ERROR': str(err)}
