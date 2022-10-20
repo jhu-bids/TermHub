aec67f6017125e3456afb5aebf80ba4b50db6d24
joeflack4 <joeflack4@gmail.com>
2022-10-20 15:53:34 -0400

    - Bugfix update to env vars /r issue

--- enclave_wrangler/config.py
+++ enclave_wrangler/config.py
@@ -15,8 +15,8 @@ TERMHUB_CSETS_DIR = os.path.join(PROJECT_ROOT, 'termhub-csets')
 
 load_dotenv(ENV_FILE)
 config = {
-    'PALANTIR_ENCLAVE_AUTHENTICATION_BEARER_TOKEN': os.getenv('PALANTIR_ENCLAVE_AUTHENTICATION_BEARER_TOKEN').replace('\r', ''),
-    'OTHER_TOKEN': os.getenv('OTHER_TOKEN').replace('\r', ''),
+    'PALANTIR_ENCLAVE_AUTHENTICATION_BEARER_TOKEN': os.getenv('PALANTIR_ENCLAVE_AUTHENTICATION_BEARER_TOKEN', '').replace('\r', ''),
+    'OTHER_TOKEN': os.getenv('OTHER_TOKEN', '').replace('\r', ''),
     'HOSTNAME': os.getenv('HOSTNAME', 'unite.nih.gov').replace('\r', ''),
     'ONTOLOGY_RID': os.getenv('ONTOLOGY_RID', 'ri.ontology.main.ontology.00000000-0000-0000-0000-000000000000').replace('\r', ''),
 }
