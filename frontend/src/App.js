087dab03a8053f3a7e1d63d88eee8a2bf8dd5501
joeflack4 <joeflack4@gmail.com>
2023-01-10 15:21:03 -0500

    Frontend devops: Added env*.js and updated github action to update API_ROOT url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,9 +22,15 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {ConceptSetsPage, CsetComparisonPage} from "./Csets";
 import {AboutPage} from "./AboutPage";
 import {searchParamsToObj} from "./utils";
+import {API_ROOT} from "./env";
+// import dotenv from 'dotenv';
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
+// const buf = Buffer.from('API_ROOT=api_root')
+// const config = dotenv.parse(buf) // will return an object
+// console.log(config)
+// const API_ROOT = 'http://127.0.0.1:8000'
 
-
-const API_ROOT = 'http://127.0.0.1:8000'
 // const enclave_url = path => `${API_ROOT}/passthru?path=${path}`
 const backend_url = path => `${API_ROOT}/${path}`
 
---
+++ frontend/src/env.js
@@ -0,0 +1,3 @@
+const API_ROOT = 'http://127.0.0.1:8000';
+
+export {API_ROOT};
