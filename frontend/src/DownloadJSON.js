2e23f3d016f4acbecbf219e58f7a197a5c17b6fa
Sigfried Gold <sigfried@jhu.edu>
2023-02-08 13:55:02 -0500

    nothing

--- frontend/src/DownloadJSON.js
+++ frontend/src/DownloadJSON.js
@@ -3,7 +3,7 @@ import {searchParamsToObj, backend_url, useDataWidget} from "./utils";
 
 function DownloadJSON(props) {
 
-  const [cset_data_widget, cset_data] = useDataWidget(codeset_ids.join('|'), cset_data_url);
+  // const [cset_data_widget, cset_data] = useDataWidget(codeset_ids.join('|'), cset_data_url);
   return (
       <h2>Nothing yet</h2>
   )
