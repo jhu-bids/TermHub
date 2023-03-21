7a82e8632d2a1044e46dfe5f11ba1461f98de2b2
Sigfried Gold <sigfried@jhu.edu>
2023-03-21 13:27:29 -0400

    just moved code around. committing so I can actually see diffs after making changes

--- frontend/src/State.js
+++ frontend/src/State.js
@@ -11,6 +11,86 @@ import {pct_fmt, } from "./utils";
 
 const DataContext = createContext(null);
 const QSContext = createContext(null);
+
+function searchParamsToObj(searchParams) {
+  const qsKeys = Array.from(new Set(searchParams.keys()));
+  let sp = {};
+  qsKeys.forEach(key => {
+    let vals = searchParams.getAll(key);
+    sp[key] = vals.map(v => parseInt(v) == v ? parseInt(v) : v).sort(); // eslint-disable-line
+    if (SEARCH_PARAM_STATE_CONFIG.scalars.includes(key)) {
+      if (sp[key].length !== 1) {
+        throw new Error("Didn't expect that!");
+      }
+      sp[key] = sp[key][0];
+    }
+    if (SEARCH_PARAM_STATE_CONFIG.serialize.includes(key)) {
+      sp[key] = JSON.parse(sp[key]);
+    }
+  });
+
+  /* if the editState has changes for a cset no longer selected, it will cause an
+      error. just get rid of those changes.
+   */
+  let fixSearchParams = {}; // don't need to do all this
+  if (sp.editCodesetId) {
+
+    if (!(sp.codeset_ids || []).includes(sp.editCodesetId)) {
+      delete sp.editCodesetId;
+      fixSearchParams.delProps = ['editCodesetId'];
+    }
+  }
+  if (sp.csetEditState) {
+    let editState = {...sp.csetEditState};
+    let update = false;
+    for (const cid in editState) {
+      if (!(sp.codeset_ids || []).includes(parseInt(cid))) {
+        delete editState[cid];
+        update = true;
+      }
+    }
+    if (update) {
+      if (isEmpty(editState)) {
+        delete sp.csetEditState;
+        fixSearchParams.delProps = [...(fixSearchParams.delProps||[]), 'csetEditState'];
+        // updateSearchParams({..._globalProps, delProps: ['csetEditState' ]});
+      } else {
+        sp.csetEditState = editState;
+        fixSearchParams.addProps = {csetEditState: editState};
+        // updateSearchParams({..._globalProps, addProps: {csetEditState: editState}});
+      }
+      //return;
+    }
+    if (!isEmpty(fixSearchParams)) {
+      // didn't need to set up fixSearchParams, just need to know if it's needed
+      sp.fixSearchParams = true;
+    }
+  }
+  // console.log({sp});
+  return sp;
+}
+function updateSearchParams(props) {
+  const {addProps={}, delProps=[], searchParams, setSearchParams, } = props;
+  let sp = searchParamsToObj(searchParams);
+  SEARCH_PARAM_STATE_CONFIG.global_props_but_not_search_params.forEach(
+      p => { delete sp[p]; } );
+  delProps.forEach( p => { delete sp[p]; } );
+  sp = {...sp, ...addProps};
+  SEARCH_PARAM_STATE_CONFIG.serialize.forEach( p => {
+    if (sp[p]) {
+      sp[p] = JSON.stringify(sp[p]);
+    }
+  })
+  const csp = createSearchParams(sp);
+  setSearchParams(csp);
+}
+function clearSearchParams(props) {
+  const {searchParams, setSearchParams, } = props;
+  const sp = searchParamsToObj(searchParams);
+  if (! isEmpty(sp)) {
+    setSearchParams(createSearchParams({}));
+  }
+}
 function Progress(props) {
   return (
     <Box sx={{ display: 'flex' }}>
@@ -86,85 +166,6 @@ function StatsMessage(props) {
     Click rows below to select or deselect concept sets.</p>
 }
 
-function searchParamsToObj(searchParams) {
-  const qsKeys = Array.from(new Set(searchParams.keys()));
-  let sp = {};
-  qsKeys.forEach(key => {
-    let vals = searchParams.getAll(key);
-    sp[key] = vals.map(v => parseInt(v) == v ? parseInt(v) : v).sort(); // eslint-disable-line
-    if (SEARCH_PARAM_STATE_CONFIG.scalars.includes(key)) {
-      if (sp[key].length !== 1) {
-        throw new Error("Didn't expect that!");
-      }
-      sp[key] = sp[key][0];
-    }
-    if (SEARCH_PARAM_STATE_CONFIG.serialize.includes(key)) {
-      sp[key] = JSON.parse(sp[key]);
-    }
-  });
-
-  /* if the editState has changes for a cset no longer selected, it will cause an
-      error. just get rid of those changes.
-   */
-  let fixSearchParams = {}; // don't need to do all this
-  if (sp.editCodesetId) {
-
-    if (!(sp.codeset_ids || []).includes(sp.editCodesetId)) {
-      delete sp.editCodesetId;
-      fixSearchParams.delProps = ['editCodesetId'];
-    }
-  }
-  if (sp.csetEditState) {
-    let editState = {...sp.csetEditState};
-    let update = false;
-    for (const cid in editState) {
-      if (!(sp.codeset_ids || []).includes(parseInt(cid))) {
-        delete editState[cid];
-        update = true;
-      }
-    }
-    if (update) {
-      if (isEmpty(editState)) {
-        delete sp.csetEditState;
-        fixSearchParams.delProps = [...(fixSearchParams.delProps||[]), 'csetEditState'];
-        // updateSearchParams({..._globalProps, delProps: ['csetEditState' ]});
-      } else {
-        sp.csetEditState = editState;
-        fixSearchParams.addProps = {csetEditState: editState};
-        // updateSearchParams({..._globalProps, addProps: {csetEditState: editState}});
-      }
-      //return;
-    }
-    if (!isEmpty(fixSearchParams)) {
-      // didn't need to set up fixSearchParams, just need to know if it's needed
-      sp.fixSearchParams = true;
-    }
-  }
-  // console.log({sp});
-  return sp;
-}
-function updateSearchParams(props) {
-    const {addProps={}, delProps=[], searchParams, setSearchParams, } = props;
-    let sp = searchParamsToObj(searchParams);
-    SEARCH_PARAM_STATE_CONFIG.global_props_but_not_search_params.forEach(
-      p => { delete sp[p]; } );
-    delProps.forEach( p => { delete sp[p]; } );
-    sp = {...sp, ...addProps};
-    SEARCH_PARAM_STATE_CONFIG.serialize.forEach( p => {
-      if (sp[p]) {
-        sp[p] = JSON.stringify(sp[p]);
-      }
-    })
-    const csp = createSearchParams(sp);
-    setSearchParams(csp);
-}
-function clearSearchParams(props) {
-  const {searchParams, setSearchParams, } = props;
-  const sp = searchParamsToObj(searchParams);
-  if (! isEmpty(sp)) {
-    setSearchParams(createSearchParams({}));
-  }
-}
 
 export {
   StatsMessage, searchParamsToObj, backend_url, axiosGet, axiosPut, useDataWidget,
