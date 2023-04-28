f1ab22a492a598bd0e712a7d6471806308e5bc2e
Sigfried Gold <sigfried@jhu.edu>
2023-04-28 08:01:42 -0400

    med-csets.csv used to make rxnorm cset csvs. forgot to add before. about to add new version before regenerating.

--- frontend/src/components/ConceptGraph.js
+++ frontend/src/components/ConceptGraph.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {dataAccessor} from "./State";
+import _ from "../supergroup/supergroup";
 
 export function currentConceptIds(props) {
   const concepts = props?.cset_data?.concepts ?? [];
@@ -28,10 +29,21 @@ export function ConceptGraph(props) {
   }, []);
 
   if (edges.length) {
-    return <pre>{JSON.stringify(edges, null, 2)}</pre>;
+    // return <pre>{JSON.stringify(edges, null, 2)}</pre>;
+    // edge looks like {
+    //     "sub": "N3C:46274124",       // child
+    //     "pred": "rdfs:subClassOf",
+    //     "obj": "N3C:36684328",       // parent
+    //     "meta": null
+    //   },
+
+    let tree = _.hierarchicalTableToTree(edges, 'obj', 'sub')
+    let paths = tree.flattenTree().map(d => d.namePath());
+    console.log(paths);
+    return <pre>{paths.join('\n')}</pre>;
   }
   if (concepts.length) {
-    return <pre>{JSON.stringify(concepts, null, 2)}</pre>;
+    // return <pre>{JSON.stringify(concepts, null, 2)}</pre>;
   }
   return <pre>{JSON.stringify(concept_ids)}</pre>;
 }
