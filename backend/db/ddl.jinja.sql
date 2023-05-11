0052fc89657ea82b74cf45ce70f5d8f083ff85e8
Sigfried Gold <sigfried@jhu.edu>
2023-05-11 08:59:23 -0400

    - moved stuff up in ddl because all_csets now depends upon it. haven't   tested it, have just been running pieces in psql as I code it.

--- backend/db/ddl.jinja.sql
+++ backend/db/ddl.jinja.sql
@@ -76,6 +76,59 @@ WHERE NOT EXISTS (
   AND csc.created_at = dd.created_at
     );
 
+DROP TABLE IF EXISTS {{schema}}cset_members_items CASCADE;
+
+CREATE TABLE {{schema}}cset_members_items AS
+SELECT
+    COALESCE(csm.codeset_id, item.codeset_id) AS codeset_id,
+    COALESCE(csm.concept_id, item.concept_id) AS concept_id,
+    csm.codeset_id IS NOT NULL AS csm,
+    item.codeset_id IS NOT NULL AS item,
+    array_to_string(array_remove(ARRAY[
+                                     CASE WHEN item."isExcluded" THEN 'isExcluded' ELSE NULL END,
+                                     CASE WHEN item."includeDescendants" THEN 'includeDescendants' ELSE NULL END,
+                                     CASE WHEN item."includeMapped" THEN 'includeMapped' ELSE NULL END ],
+                                 NULL), ',') AS item_flags,
+    item."isExcluded",
+    item."includeDescendants",
+    item."includeMapped"
+FROM {{schema}}concept_set_members csm
+FULL OUTER JOIN {{schema}}concept_set_version_item item
+ON csm.codeset_id = item.codeset_id
+    AND csm.concept_id = item.concept_id
+WHERE csm.codeset_id IS NOT NULL
+   OR item.codeset_id IS NOT NULL;
+
+CREATE INDEX csmi_idx1 ON {{schema}}cset_members_items(codeset_id);
+
+CREATE INDEX csmi_idx2 ON {{schema}}cset_members_items(concept_id);
+
+CREATE INDEX csmi_idx3 ON {{schema}}cset_members_items(codeset_id, concept_id);
+
+DROP TABLE IF EXISTS {{schema}}members_items_summary;
+
+CREATE TABLE {{schema}}members_items_summary AS
+SELECT
+    codeset_id,
+    CASE WHEN item THEN 'Expression item -- '||
+                        CASE WHEN LENGTH(item_flags) > 0 THEN item_flags ELSE 'no flags' END || '. '
+         ELSE '' END ||
+    CASE WHEN csm THEN 'Is a member' ELSE 'Is not a member' END AS grp,
+    COUNT(*) AS cnt
+FROM {{schema}}cset_members_items
+GROUP by 1,2
+UNION
+SELECT codeset_id, 'Members' AS grp, COUNT(*) AS cnt FROM cset_members_items WHERE csm GROUP by 1,2
+UNION
+SELECT codeset_id, 'Expression items' AS grp, COUNT(*) AS cnt FROM cset_members_items WHERE item GROUP by 1,2;
+
+CREATE INDEX mis1 on {{schema}}members_items_summary(codeset_id);
+
+CREATE TABLE {{schema}}codeset_counts AS
+SELECT codeset_id, JSON_AGG(JSON_BUILD_OBJECT('grp', grp, 'cnt', cnt)) AS counts FROM {{schema}}members_items_summary GROUP BY 1;
+
+CREATE INDEX csc1 on {{schema}}codeset_counts(codeset_id);
+
 
 DROP TABLE IF EXISTS all_csets;
 
@@ -136,59 +189,6 @@ LEFT JOIN {{schema}}codeset_counts cscnt ON ac.codeset_id = cscnt.codeset_id;
 CREATE INDEX ac_idx1 ON {{schema}}all_csets(codeset_id);
 
 CREATE INDEX ac_idx2 ON {{schema}}all_csets(concept_set_name);
-
-DROP TABLE IF EXISTS {{schema}}cset_members_items CASCADE;
-
-CREATE TABLE {{schema}}cset_members_items AS
-SELECT
-        COALESCE(csm.codeset_id, item.codeset_id) AS codeset_id,
-        COALESCE(csm.concept_id, item.concept_id) AS concept_id,
-        csm.codeset_id IS NOT NULL AS csm,
-        item.codeset_id IS NOT NULL AS item,
-        array_to_string(array_remove(ARRAY[
-              CASE WHEN item."isExcluded" THEN 'isExcluded' ELSE NULL END,
-              CASE WHEN item."includeDescendants" THEN 'includeDescendants' ELSE NULL END,
-              CASE WHEN item."includeMapped" THEN 'includeMapped' ELSE NULL END ],
-            NULL), ',') AS item_flags,
-        item."isExcluded",
-        item."includeDescendants",
-        item."includeMapped"
-FROM {{schema}}concept_set_members csm
-FULL OUTER JOIN {{schema}}concept_set_version_item item
-   ON csm.codeset_id = item.codeset_id
-  AND csm.concept_id = item.concept_id
-WHERE csm.codeset_id IS NOT NULL
-   OR item.codeset_id IS NOT NULL;
-
-CREATE INDEX csmi_idx1 ON {{schema}}cset_members_items(codeset_id);
-
-CREATE INDEX csmi_idx2 ON {{schema}}cset_members_items(concept_id);
-
-CREATE INDEX csmi_idx3 ON {{schema}}cset_members_items(codeset_id, concept_id);
-
-DROP TABLE IF EXISTS {{schema}}members_items_summary;
-
-CREATE TABLE {{schema}}members_items_summary AS
-SELECT
-      codeset_id,
-      CASE WHEN item THEN 'Expression item -- '||
-        CASE WHEN LENGTH(item_flags) > 0 THEN item_flags ELSE 'no flags' END || '. '
-        ELSE '' END ||
-        CASE WHEN csm THEN 'Is a member' ELSE 'Is not a member' END AS grp,
-      COUNT(*) AS cnt
-FROM {{schema}}cset_members_items
-GROUP by 1,2
-UNION
-SELECT codeset_id, 'Members' AS grp, COUNT(*) AS cnt FROM cset_members_items WHERE csm GROUP by 1,2
-UNION
-SELECT codeset_id, 'Expression items' AS grp, COUNT(*) AS cnt FROM cset_members_items WHERE item GROUP by 1,2;
-
-CREATE INDEX mis1 on {{schema}}members_items_summary(codeset_id);
-
-CREATE TABLE {{schema}}codeset_counts AS
-SELECT codeset_id, JSON_AGG(JSON_BUILD_OBJECT('grp', grp, 'cnt', cnt)) AS counts FROM {{schema}}members_items_summary GROUP BY 1;
-
-CREATE INDEX csc1 on {{schema}}codeset_counts(codeset_id);
 
 CREATE OR REPLACE VIEW {{schema}}cset_members_items_plus AS (
 SELECT    csmi.*
