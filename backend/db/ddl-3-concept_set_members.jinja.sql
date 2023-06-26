a43f8429fa7b6f3a5b6c5868be51aa65f1f47341
Sigfried Gold <sigfried@jhu.edu>
2023-06-26 12:00:16 -0400

    @joeflack4: Had to change the ddl scripts. Can you put the new code I added to ddl-3-concept_set_members.jinja.sql where you want it? But it has to go before any derived tables are created based on concept_set_members or concept_set_version_item. (They have records with no associated code_sets row, which is creating problems)

--- backend/db/ddl-3-concept_set_members.jinja.sql
+++ backend/db/ddl-3-concept_set_members.jinja.sql
@@ -8,3 +8,14 @@ DROP TABLE {{schema}}concept_set_members;
 SELECT DISTINCT * INTO {{schema}}concept_set_members FROM {{schema}}concept_set_members_with_dups;
 
 DROP TABLE concept_set_members_with_dups;
+
+-- there are version items with no corresponding row in the code_sets table!!
+-- have to get rid of them
+-- @joeflack4, this code needs to go early...not sure where you want it
+DELETE FROM concept_set_version_item vi
+WHERE NOT EXISTS (
+    SELECT * FROM code_sets cs WHERE cs.codeset_id = vi.codeset_id);
+
+DELETE FROM concept_set_members csm
+WHERE NOT EXISTS (
+    SELECT * FROM code_sets cs WHERE cs.codeset_id = csm.codeset_id);
