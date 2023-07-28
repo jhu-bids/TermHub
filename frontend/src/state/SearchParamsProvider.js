4ed3c3a1409b868c2223349b059f1f8a39c2cfc9
Sigfried Gold <sigfried@sigfried.org>
2023-07-28 09:22:13 -0400

    - Change hard-coded 0 for editCsetId to constant NEW_CSET_ID = -1 - Added persistedReducer stuff so we can use reducers and save state to   either localStorage or url. Maybe this could replace DataCache.js? And   most of SearchParamsProvider.js?   - Had to do weird thing by allowing default state instead of (or     in addition to) initial state because updating url for default     (initial) values of hierarchySettings was making extra rerenders and     maybe causing problems. Instead only need to persist differences     from default state, but, when accessing state, see the default with     the changes applied:  state = {...unpersistedDefaultState, ...state}

--- frontend/src/state/SearchParamsProvider.js
+++ frontend/src/state/SearchParamsProvider.js
@@ -1,51 +1,93 @@
-import React, {createContext, useContext, } from "react";
-import {createSearchParams, useSearchParams, useLocation, Navigate, } from "react-router-dom";
-import {isEmpty} from "lodash";
+import React, {createContext, useContext, useState, useCallback, } from "react";
+import {createSearchParams, useSearchParams, /* useLocation, Navigate, */ } from "react-router-dom";
+// import {isEmpty} from "lodash";
 
 const SEARCH_PARAM_STATE_CONFIG = {
   scalars: ["editCodesetId", "sort_json", "use_example"],
   global_props_but_not_search_params: [], // ["searchParams", "setSearchParams"],
-  serialize: ["csetEditState"],
+  serialize: ["csetEditState", "hierarchySettings"],
 };
 
 const SearchParamsContext = createContext(null);
 
 export function SearchParamsProvider({children}) {
   const [searchParams, setSearchParams] = useSearchParams();
-  const location = useLocation();
+
+  const searchParamsToObj = useCallback(searchParams => {
+    const qsKeys = Array.from(new Set(searchParams.keys()));
+    let sp = {};
+    qsKeys.forEach((key) => {
+      let vals = searchParams.getAll(key);
+      sp[key] = vals.map((v) => (parseInt(v) == v ? parseInt(v) : v)).sort(); // eslint-disable-line
+      if (SEARCH_PARAM_STATE_CONFIG.scalars.includes(key)) {
+        if (sp[key].length !== 1) {
+          throw new Error("Didn't expect that!");
+        }
+        sp[key] = sp[key][0];
+      }
+      if (SEARCH_PARAM_STATE_CONFIG.serialize.includes(key)) {
+        sp[key] = JSON.parse(sp[key]);
+      }
+    });
+    console.log('returning sp', sp);
+    return sp;
+  }, [searchParams]);
+
+  let sp = searchParamsToObj(searchParams);
+  console.log('got sp', sp);
+
   // gets state (codeset_ids for now) from query string, passes down through props
   // const [codeset_ids, setCodeset_ids] = useState(sp.codeset_ids || []);
-  let sp = searchParamsToObj(searchParams, setSearchParams);
+
   const { codeset_ids = [] } = sp;
 
-  if (sp.fixSearchParams) {
-    debugger; // is this code still needed?
-    delete sp.fixSearchParams;
-    const csp = createSearchParams(sp);
-    return <Navigate to={location.pathname + "?" + csp.toString()} />;
-  }
-
   function updateSearchParams(props) {
-    const {addProps = {}, delProps = [], } = props;
-    let sp = searchParamsToObj(searchParams);
+    const {addProps = {}, delProps = [], replaceAllProps} = props;
+    let sp;
+    if (replaceAllProps) {
+      sp = replaceAllProps;
+    } else {
+      sp = searchParamsToObj(searchParams);
+    }
     /* SEARCH_PARAM_STATE_CONFIG.global_props_but_not_search_params.forEach((p) => { delete sp[p]; }); */
     delProps.forEach((p) => {
       delete sp[p];
     });
     sp = {...sp, ...addProps};
     SEARCH_PARAM_STATE_CONFIG.serialize.forEach((p) => {
-      if (sp[p]) {
+      if (sp[p] && typeof(sp[p] !== 'string')) {
         sp[p] = JSON.stringify(sp[p]);
       }
     });
     const csp = createSearchParams(sp);
-    setSearchParams(csp);
+    if (csp+'' !== searchParams+'') {
+      console.log('setting searchParams', csp+'');
+      setSearchParams(csp);
+    }
   }
 
+  // getItem and setItem are so SearchParams can be used with usePersistedReducer
+  function getItem(key) {
+    let sp = searchParamsToObj(searchParams);
+    return sp[key] ?? null ;
+  }
+
+  function setItem(key, value) {
+    /*
+    if (typeof(value) === 'string') {
+      value = JSON.stringify(value);
+    }
+     */
+    updateSearchParams({addProps: {[key]: value}});
+  /**/}
+
   function changeCodesetIds(codeset_id, how) {
+    let sp = searchParamsToObj(searchParams);
+
     // how = add | remove | toggle
     if (how === "set" && Array.isArray(codeset_id)) {
-      updateSearchParams({ ...sp, addProps: { codeset_ids: codeset_id }, searchParams, setSearchParams, });
+      updateSearchParams({ /* ...sp, haven't tested removing this, but should be ok */
+        addProps: { codeset_ids: codeset_id }, searchParams, setSearchParams, });
       return;
     }
     const included = codeset_ids.includes(codeset_id);
@@ -73,7 +115,9 @@ export function SearchParamsProvider({children}) {
   if (!sp.codeset_ids) {
     sp.codeset_ids = [];
   }
-  const value = {sp, updateSp: updateSearchParams, changeCodesetIds, };
+  const value = {
+    sp, updateSp: updateSearchParams, changeCodesetIds,
+    getItem, setItem, dontStringifySetItem: true, };
   return (
       <SearchParamsContext.Provider value={value} >
         {children}
@@ -86,64 +130,58 @@ export function useSearchParamsState() {
 }
 
 
-export function searchParamsToObj(searchParams) {
-  const qsKeys = Array.from(new Set(searchParams.keys()));
-  let sp = {};
-  qsKeys.forEach((key) => {
-    let vals = searchParams.getAll(key);
-    sp[key] = vals.map((v) => (parseInt(v) == v ? parseInt(v) : v)).sort(); // eslint-disable-line
-    if (SEARCH_PARAM_STATE_CONFIG.scalars.includes(key)) {
-      if (sp[key].length !== 1) {
-        throw new Error("Didn't expect that!");
-      }
-      sp[key] = sp[key][0];
-    }
-    if (SEARCH_PARAM_STATE_CONFIG.serialize.includes(key)) {
-      sp[key] = JSON.parse(sp[key]);
-    }
-  });
-
-  /* if the editState has changes for a cset no longer selected, it will cause an
-      error. just get rid of those changes.
-   */
-  let fixSearchParams = {}; // don't need to do all this
-  if (sp.editCodesetId) {
-    if (!(sp.codeset_ids || []).includes(sp.editCodesetId)) {
-      delete sp.editCodesetId;
-      fixSearchParams.delProps = ["editCodesetId"];
-    }
-  }
   /*
-  if (sp.csetEditState) {
-    let editState = {...sp.csetEditState};
-    let update = false;
-    for (const cid in editState) {
-      if (!(sp.codeset_ids || []).includes(parseInt(cid))) {
-        delete editState[cid];
-        update = true;
+
+  was near the top of SearchParamsProvider:
+    const location = useLocation();
+    if (sp.fixSearchParams) {
+      debugger; // is this code still needed?
+      delete sp.fixSearchParams;
+      const csp = createSearchParams(sp);
+      return <Navigate to={location.pathname + "?" + csp.toString()} />;
+    }
+
+  this was at the bottom of searchParamsToObj:
+    if the editState has changes for a cset no longer selected, it will cause an
+      error. just get rid of those changes.
+
+
+    let fixSearchParams = {}; // don't need to do all this
+    if (sp.editCodesetId) {
+      if (!(sp.codeset_ids || []).includes(sp.editCodesetId)) {
+        delete sp.editCodesetId;
+        fixSearchParams.delProps = ["editCodesetId"];
       }
     }
-    if (update) {
-      if (isEmpty(editState)) {
-        delete sp.csetEditState;
-        fixSearchParams.delProps = [
-          ...(fixSearchParams.delProps || []),
-          "csetEditState",
-        ];
-        // updateSearchParams({..._globalProps, delProps: ['csetEditState' ]});
-      } else {
-        sp.csetEditState = editState;
-        fixSearchParams.addProps = {csetEditState: editState};
-        // updateSearchParams({..._globalProps, addProps: {csetEditState: editState}});
+
+  this was at the bottom of searchParamsToObj but commented out already:
+    if (sp.csetEditState) {
+      let editState = {...sp.csetEditState};
+      let update = false;
+      for (const cid in editState) {
+        if (!(sp.codeset_ids || []).includes(parseInt(cid))) {
+          delete editState[cid];
+          update = true;
+        }
       }
-      //return;
+      if (update) {
+        if (isEmpty(editState)) {
+          delete sp.csetEditState;
+          fixSearchParams.delProps = [
+            ...(fixSearchParams.delProps || []),
+            "csetEditState",
+          ];
+          // updateSearchParams({..._globalProps, delProps: ['csetEditState' ]});
+        } else {
+          sp.csetEditState = editState;
+          fixSearchParams.addProps = {csetEditState: editState};
+          // updateSearchParams({..._globalProps, addProps: {csetEditState: editState}});
+        }
+        //return;
+      }
+      if (!isEmpty(fixSearchParams)) {
+        // didn't need to set up fixSearchParams, just need to know if it's needed
+        sp.fixSearchParams = true;
+      }
     }
-    if (!isEmpty(fixSearchParams)) {
-      // didn't need to set up fixSearchParams, just need to know if it's needed
-      sp.fixSearchParams = true;
-    }
-  }
-   */
-  // console.log({sp});
-  return sp;
-}
+ */
--- frontend/src/state/usePersistedReducer.js
+++ frontend/src/state/usePersistedReducer.js
@@ -1,35 +1,68 @@
-import React, { useEffect, useReducer } from 'react';
+// start with code from https://github.com/johnayeni/use-persisted-reducer
 
-export const usePersistedReducer = (reducer, initialState, init, key, storage) => {
-  const [state, dispatch] = React.useReducer(reducer, storage.get(key, initialState), init);
+import React, { useEffect, useReducer } from 'react';
+import {get} from 'lodash';
+
+/*
+  - Had to do weird thing by allowing default state instead of (or
+    in addition to) initial state because updating url for default
+    (initial) values of hierarchySettings was making extra rerenders and
+    maybe causing problems. Instead only need to persist differences
+    from default state, but, when accessing state, see the default with
+    the changes applied:  state = {...unpersistedDefaultState, ...state}
+ */
+export const usePersistedReducer = (reducer, initialState, key, storage, unpersistedDefaultState) => {
+  let [state, dispatch] = React.useReducer(reducer, storage.get(key, initialState));
 
   useEffect(() => {
     storage.set(key, state);
   }, [state]);
 
+  if (unpersistedDefaultState) {
+    state = {...unpersistedDefaultState, ...state};
+  }
   return [state, dispatch];
 };
 
-const createStorage = (provider) => ({
+// meant to be usable with localStorage or url query param storage, which needs
+//  to be small and acts weird when trying to throw initialState onto it, so
+//  allowing unpersistedDefaultState as an alternative
+const createStorage = (provider, unpersistedDefaultState) => ({
   get(key, initialState) {
+    let ret;
     const json = provider.getItem(key);
-    let ret = json === null
-        ? typeof initialState === 'function'
-            ? initialState()
-            : initialState
-        : JSON.parse(json);
-    console.log(`get(${key}, ${initialState}`, ret);
+    try {
+      ret = json === null
+          ? typeof initialState === 'function'
+              ? initialState()
+              : initialState
+          : JSON.parse(json);
+    } catch(err) {
+      if (err.name === 'SyntaxError') {
+        ret = json;
+      } else {
+        throw err;
+      }
+    }
+    // console.log(`get(${key}, ${initialState}) with ${unpersistedDefaultState}`, ret);
+    return ret ?? (unpersistedDefaultState || {})[key];
   },
   set(key, value) {
-    provider.setItem(key, JSON.stringify(value));
+    if (get(unpersistedDefaultState, key) == value) {
+      return;  // trying to set something already in the default
+    }
+    if (!provider.dontStringifySetItem) {
+      value = JSON.stringify(value);
+    }
+    provider.setItem(key, value);
   },
 });
 
-export const createPersistedReducer = (key, provider = /* globalThis. */localStorage) => {
+export const createPersistedReducer = (key, provider, unpersistedDefaultState) => {
   if (provider) {
-    const storage = createStorage(provider);
-    return (reducer, initialState, init) =>
-      usePersistedReducer(reducer, initialState, init, key, storage);
+    const storage = createStorage(provider, unpersistedDefaultState);
+    return (reducer, initialState) =>
+      usePersistedReducer(reducer, initialState, key, storage, unpersistedDefaultState);
   }
   return useReducer;
 };
