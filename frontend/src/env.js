f41ef04e9a0c540dbd8c071d2de8076221dbcb98
Sigfried Gold <sigfried@sigfried.org>
2023-08-07 11:27:38 -0400

    - Added new variables to env files. - Moved version number from version.js to env.js -@joeflack4, can you look at this and make sure it's ok with you?

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -4,6 +4,9 @@
 // dotenv.config()
 // const config = dotenv.parse(buf) // will return an object
 // const buf = Buffer.from('API_ROOT=api_root')
-// const API_ROOT = 'http://127.0.0.1:8000'
 
 export const API_ROOT = "http://127.0.0.1:8000";
+export const SOURCE_APPLICATION = "TermHub";
+export const SOURCE_APPLICATION_VERSION = "0.3.0";
+export const VERSION = SOURCE_APPLICATION_VERSION;
+export const SERVICE_USER_ID = "6387db50-9f12-48d2-b7dc-e8e88fdf51e3";
