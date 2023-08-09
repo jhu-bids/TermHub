f113e7e0ab7fc2b56003c2cb1881314bf713d655
Sigfried Gold <sigfried@sigfried.org>
2023-08-09 06:25:27 -0400

    API_ROOT was working on dev and prod but not local. fixed that.

--- frontend/src/env.dev.js
+++ frontend/src/env.dev.js
@@ -1,3 +1,2 @@
 // dev API_ROOT
-API_ROOT = "https://termhub-dev.azurewebsites.net";
-export {API_ROOT};
+export const API_ROOT = "https://termhub-dev.azurewebsites.net";
--- frontend/src/env.prod.js
+++ frontend/src/env.prod.js
@@ -1,3 +1,2 @@
 // prod API_ROOT
-API_ROOT = "https://termhub.azurewebsites.net";
-export {API_ROOT};
+export const API_ROOT = "https://termhub.azurewebsites.net";
