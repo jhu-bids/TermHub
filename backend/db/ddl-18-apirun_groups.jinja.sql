00e29d094013a2577bf9fc3cb0d83aeabc96d872
Sigfried Gold <sigfried@sigfried.org>
2024-03-18 08:31:50 -0400

    - More changes in ddl-18-api stuff. Now have table that is probably the   best to use as basis for analysis: apijoin. @joeflack4, lets go over   it a bit and rerun everything based on it.

--- backend/db/ddl-18-apirun_groups.jinja.sql
+++ backend/db/ddl-18-apirun_groups.jinja.sql
@@ -22,20 +22,44 @@ DROP SEQUENCE IF EXISTS public.api_call_group_id_seq;
 
 CREATE SEQUENCE public.api_call_group_id_seq START 10001;
 
-DROP TABLE IF EXISTS public.apiruns_grouped CASCADE;
+DROP TABLE IF EXISTS public.apiruns_plus CASCADE;
 
 WITH RankedGroups AS (
         SELECT
             *,
-            ROW_NUMBER() OVER (PARTITION BY api_call_group_id ORDER BY timestamp::timestamp DESC) AS rn
+            substring(client from '\d+\.\d+\.\d+') AS ip3, -- sometimes fourth ip part differs for same call session
+            ROW_NUMBER() OVER (PARTITION BY api_call_group_id ORDER BY timestamp::timestamp DESC) AS rn,
+            EXTRACT(SECOND FROM timestamp::timestamp - (
+                lag(timestamp::timestamp) OVER (PARTITION by host, substring(client from '\d+\.\d+\.\d+'), api_call_group_id ORDER BY timestamp) +
+                    lag(process_seconds) OVER (PARTITION by host, substring(client from '\d+\.\d+\.\d+'), api_call_group_id ORDER BY timestamp)
+                    * INTERVAL '1 second')) AS call_gap,
+            CASE
+              WHEN timestamp::timestamp - LAG(timestamp::timestamp) OVER
+                  (PARTITION BY host, substring(client from '\d+\.\d+\.\d+'), api_call_group_id ORDER BY timestamp::timestamp) > INTERVAL '1 second' THEN 1
+              ELSE 0
+            END AS new_group_flag
+
         FROM public.api_runs
-        WHERE api_call_group_id IS NOT NULL
-    )
+        WHERE host IN ('prod', 'dev')
+          AND client NOT LIKE '216.164.48.98%'
+          AND client NOT LIKE '174.99.54.40%'
+          AND client NOT LIKE '136.226%' -- this one can differ by the third and fourth ip parts; just skipping it.
+    ),
+groups_broke_by_long_gap AS (
+  SELECT *,
+    host || '-' || ip3 || '-' || COALESCE(api_call_group_id::text, 'no_call_grp') || '-' ||
+        SUM(new_group_flag) OVER (PARTITION BY host, ip3, api_call_group_id
+            ORDER BY timestamp::timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS group_id
+  FROM RankedGroups
+)
+SELECT * INTO public.apiruns_plus FROM groups_broke_by_long_gap;
+
+CREATE INDEX aprpidx ON apiruns_plus (group_id );
+
+DROP TABLE IF EXISTS public.apiruns_grouped CASCADE;
+
 SELECT
-    host,
-    client,
-    schema,
-    api_call_group_id,
+    group_id,
     /*
     codeset_ids,
     params,
@@ -47,25 +71,28 @@ SELECT
     CASE WHEN MAX(rn) = 1 THEN MAX(process_seconds) * INTERVAL '1 second' ELSE INTERVAL '0 seconds' END as duration_seconds
 INTO public.apiruns_grouped
 FROM
-    RankedGroups
+    public.apiruns_plus
 GROUP BY
-    api_call_group_id, host, client, schema /* , api_call, codeset_ids, params*/
-ORDER BY api_call_group_id desc; -- group_start_time DESC, client
+    group_id /* , api_call, codeset_ids, params*/
+ORDER BY group_id desc; -- group_start_time DESC, ip3
 
-CREATE INDEX aprgidx ON apiruns_grouped(api_call_group_id);
+CREATE INDEX aprgidx ON apiruns_grouped(group_id);
 
 DROP TABLE IF EXISTS public.apijoin CASCADE;
 
-SELECT DISTINCT r.*, array_sort(g.api_calls) api_calls, g.duration_seconds, g.group_start_time,
+SELECT DISTINCT r.*,
+                array_sort(g.api_calls) api_calls, g.duration_seconds, g.group_start_time,
                 date_bin('1 week', timestamp::TIMESTAMP, TIMESTAMP '2023-10-30')::date week,
                 timestamp::date date,
-                FORMAT('%s of %s', ROW_NUMBER() OVER(PARTITION BY g.api_call_group_id ORDER BY timestamp), COUNT(*) OVER(PARTITION BY g.api_call_group_id)) AS callnum
+                FORMAT('%s of %s',
+                       ROW_NUMBER() OVER(PARTITION BY r.group_id ORDER BY timestamp),
+                       COUNT(*) OVER(PARTITION BY r.group_id)) AS callnum
 INTO public.apijoin
-FROM public.api_runs r
-LEFT JOIN public.apiruns_grouped g ON g.api_call_group_id = r.api_call_group_id AND g.api_call_group_id != -1
+FROM public.apiruns_plus r
+LEFT JOIN public.apiruns_grouped g ON g.group_id = r.group_id
 ;
 
-CREATE INDEX aprjidx ON public.apijoin(api_call_group_id);
+CREATE INDEX aprjidx ON public.apijoin(group_id);
 
 /*
 WITH RankedRows AS (
