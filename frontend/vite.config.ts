1eee6261c756911759d034de7c94d74939aec720
Sigfried Gold <sigfried@sigfried.org>
2024-05-23 12:48:19 -0400

    - Was trying to get react profiler workihng. spent hours, no luck.   leaving some of the code because it might be good to have for working   with dev/prod environment variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
-
 import {execSync} from 'child_process';
+
+
 function getCommitHash() {
   try {
     return execSync('git rev-parse --short HEAD').toString().trim();
@@ -12,17 +13,45 @@ function getCommitHash() {
   }
 }
 
-export default defineConfig({
-    // depending on your application, base can also be "/"
+// from https://chatgpt.com/share/9ce9cab9-f346-427b-85a2-11937f566e73 React Timeline Profiler Setup
+export default ({ mode }) => {
+  console.log('mode:', mode);
+  // Load environment variables from .env file
+  const env = loadEnv(mode, process.cwd());
+
+  console.log('NODE_ENV:', process.env.NODE_ENV);
+  // Log the environment variable to verify it's loaded
+  console.log('VITE_REACT_PROFILING:', env.VITE_REACT_PROFILING);
+
+  // Determine if profiling should be enabled
+  const isProfiling = env.VITE_REACT_PROFILING === 'true';
+
+  return defineConfig({
     base: '',
     plugins: [react(), viteTsconfigPaths()],
-    server: {
-        // this ensures that the browser opens upon server start
-        open: true,
-        // this sets a default port to 3000
-        port: 3000,
-    },
+    // resolve: {
+    //   alias: {
+    //     'react-dom': isProfiling ? 'react-dom/profiling' : 'react-dom',
+    //     'scheduler/tracing': isProfiling ? 'scheduler/tracing-profiling' : 'scheduler/tracing',
+    //   },
+    // },
     define: {
-        'process.env.COMMIT_HASH': JSON.stringify(getCommitHash())
-    }
-})
+      'process.env.COMMIT_HASH': JSON.stringify(getCommitHash()),
+    },
+    server: {
+      // whether the browser opens upon server start
+      open: false,
+      // this sets a default port to 3000
+      port: 3000,
+    },
+    // build: {
+    //   outDir: 'dist',
+    //   rollupOptions: {
+    //     input: {
+    //       main: '/index.html',
+    //       report: '/playwright-report/index.html'
+    //     }
+    //   }
+    // },
+  });
+};
